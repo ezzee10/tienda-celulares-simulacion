5c646cdfdfa96b786ac07b218911031cdf508803
Ezequiel <ezequiel.colombano@gmail.com>
2020-06-27 23:03:35 -0300

    consumiendo los datos de la bd

--- index.php
+++ index.php
@@ -8,25 +8,30 @@
   <div class="container">
     <main class="contenido">
       <h2>Escoja el modelo que desea comprar</h2>
-    </main>
 
-    <?php 
+      <?php 
     
-        $productos = obtenerProducto();
-        //echo var_dump($productos);
-        if($productos ->num_rows){
-            foreach($productos as $producto){
-              echo '<pre>';
-              var_dump($producto);
-              echo '</pre>';
-            }
-        }
+        $productos = obtenerProducto(); 
+        
+        ?>
 
-    ?>
-
-
-
+        <div class="productos">
+      <?php foreach($productos as $producto) { ?>
+            <div class="producto">
+                <img src="<?php echo $producto['foto_producto']?>">
+                <p> <?php echo $producto["nombre_producto"] ?> </p>
+                <p> <?php echo $producto["marca_producto"] ?> </p>
+                <p> <?php echo $producto["precio_producto"] ?> </p>
+                <button>Comprar</button>
+            </div>
+           <?php } ?>
+      </div>
+    </main>
   </div>
+
+   <?php
+    $conn->close();
+    ?>
 
 <?php
     include 'includes/layout/footer.php';
